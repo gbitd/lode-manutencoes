cc08de70a08f04b06292fe9074938592df53dfdc
gbitd <gbitdinger@proton.me>
2026-05-11 15:01:14 -0300

    Primeiro commit: Inicializando projeto SpringBoot

---
+++ src/main/java/com/gbitd/lode_manutencoes/LodeManutencoesApplication.java
@@ -0,0 +1,13 @@
+package com.gbitd.lode_manutencoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodeManutencoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodeManutencoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbitd/lode_manutencoes/LodeManutencoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbitd.lode_manutencoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodeManutencoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
