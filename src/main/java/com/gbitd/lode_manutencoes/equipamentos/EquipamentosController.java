a66e3e28dd97fffeac51846eeabce43282e77128
gbitd <gbitdinger@proton.me>
2026-05-12 15:41:32 -0300

    Refatoração: Renomeia ordemdeservico -> equipamentos

--- src/main/java/com/gbitd/lode_manutencoes/equipamentos/EquipamentosController.java
+++ src/main/java/com/gbitd/lode_manutencoes/equipamentos/EquipamentosController.java
@@ -1,4 +1,4 @@
-package com.gbitd.lode_manutencoes.ordensdeservico;
+package com.gbitd.lode_manutencoes.equipamentos;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class OrdensDeServicoController {
+public class EquipamentosController {
     @GetMapping
     public String hello(){
         return "Tudo funcionando!";
